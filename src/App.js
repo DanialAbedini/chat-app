d5e53e1119b19d64e54c90bc00656f06448dbcbe
danial <danial@gmail.com>
2021-11-19 18:05:56 +0330

    context added to pj

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Route, Switch, Redirect } from "react-router-dom";
 
 //! component
 import Login from "./components/Login";
+import Chats from "./components/Chats";
+
+//!context
+import AuthContextProvider from "./contexts/AuthContextProvider";
 const App = () => {
   return (
     <div>
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Redirect path="/" to="/login" />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/chats" component={Chats} />
+          <Redirect path="/" to="/login" />
+        </Switch>
+      </AuthContextProvider>
     </div>
   );
 };
